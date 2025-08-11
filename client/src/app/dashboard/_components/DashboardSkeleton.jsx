701a1d13a3f0f395bf1c46ddffc74c7fa36e5d2c
UmaGhildiyal <ghildiyaluma0@gmail.com>
2025-08-12 00:06:08 +0530

    feat: implement DashboardSkeleton and DashboardLayout components for improved dashboard UI

---
+++ client/src/app/dashboard/_components/DashboardSkeleton.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="flex w-full">
+      <div className="flex h-full w-full flex-col gap-2 rounded-tl-2xl border border-zinc-320 bg-white p-2 md:p-10 dark:border-zinc-700 dark:bg-zinc-950">
+        <div className="flex gap-2">
+          {[...new Array(4)].map((_, idx) => (
+            <div
+              key={"first-array-demo-1" + idx}
+              className="h-32 w-full animate-pulse rounded-lg bg-gray-100 dark:bg-zinc-800"
+            ></div>
+          ))}
+        </div>
+        <div className="flex gap-2">
+          {[...new Array(1)].map((_, idx) => (
+            <div
+              key={"first-array-demo-1" + idx}
+              className="h-96 w-full animate-pulse rounded-lg bg-gray-100 dark:bg-zinc-800"
+            ></div>
+          ))}
+        </div>
+        <div className="flex gap-2">
+          {[...new Array(2)].map((_, idx) => (
+            <div
+              key={"first-array-demo-1" + idx}
+              className="h-32 w-full animate-pulse rounded-lg bg-gray-100 dark:bg-zinc-800"
+            ></div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- client/src/app/dashboard/layout.jsx
+++ client/src/app/dashboard/layout.jsx
@@ -1,23 +1,20 @@
-import React from "react";
-import Navbar from "./components/Navbar.jsx";
-import SideNav from "./components/SideNav.jsx";
+import Navbar from "../components/Navbar";
+import SideNav from "../components/SideNav";
 
-const DashboardWrapper = ({ children }) => {
+
+const DashboardLayout = ({ children }) => {
   return (
-    
     <div className="flex w-full flex-1 flex-col overflow-hidden rounded-md border border-zinc-200 bg-gray-100 md:flex-row dark:border-zinc-700 dark:bg-zinc-800 h-screen">
-      {/* Sidebar */}
+      
       <SideNav />
 
-      {/* Main content area - takes remaining width */}
       <div className="flex flex-1 w-full">
         <div className="flex h-full w-full flex-1 flex-col rounded-tl-2xl border border-zinc-200 bg-white dark:border-zinc-700 dark:bg-zinc-950">
-          {/* Navbar */}
+          
           <div className="pt-4 pl-10 pb-0 border-zinc-200 dark:border-zinc-700">
             <Navbar />
           </div>
 
-          {/* Content - full width */}
           <div className="flex-1 overflow-auto p-4 md:p-10 md:pt-4 w-full">
             {children}
           </div>
@@ -27,4 +24,4 @@ const DashboardWrapper = ({ children }) => {
   );
 };
 
-export default DashboardWrapper;
+export default DashboardLayout;
